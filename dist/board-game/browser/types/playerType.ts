60528036c178da48a352de8d377f5dc2d80eb9cf
Felipe Santos <fsantos29004@gmail.com>
2024-11-06 00:13:29 -0300

    Build for Production

---
+++ dist/board-game/browser/types/playerType.ts
@@ -0,0 +1,7 @@
+export type Player = {
+    id: number,
+    color: string,
+    position: number,
+    points: number,
+    laps: number
+}
---
+++ dist/board-game/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-KW6KDCF4.mjs";import"./chunk-MU262CQZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
