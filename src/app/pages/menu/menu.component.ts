ec7d6018762a5401811f705b58ccf8430d8a3ed2
Felipe Santos <fsantos29004@gmail.com>
2024-11-07 00:54:09 -0300

    Add new styles to buttons and inputs

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
@@ -14,6 +15,7 @@ export class MenuComponent {
   constructor( private route: Router ){}
 
   iniciarJogo(numPlayers: string) {
+    this.soundClick()
     const numPlrs = Number(numPlayers)
 
     if(numPlrs >= 2 && numPlrs <= 4){
@@ -22,10 +24,17 @@ export class MenuComponent {
   }
 
   openModal(modalId: string) {
+    this.soundClick()
     this.modalActive = modalId;
 }
 
   closeModal() {
+    this.soundClick()
     this.modalActive = null;
+  }
+
+  soundClick() {
+    const click = new Audio('sounds/click.mp3')
+    click.play()
   }
 }
