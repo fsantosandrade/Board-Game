e144763495b153bce01b0f07212230797c42e534
Felipe Santos <fsantos29004@gmail.com>
2024-11-07 00:59:05 -0300

    Build for Production

--- dist/board-game/server/main.server.mjs
+++ dist/board-game/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6QX7IN6F.mjs";import"./chunk-WLIIJXI6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-HBWIQGWM.mjs";import"./chunk-WLIIJXI6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
